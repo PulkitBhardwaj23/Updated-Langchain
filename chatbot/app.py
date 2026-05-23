afe4fb2a4632c757e6bf09d62a1f74bd2d16b373
PulkitBhardwaj23 <168618190+PulkitBhardwaj23@users.noreply.github.com>
2026-05-23 21:31:07 +0530

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,34 +1,34 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI  # communicate with open ai models
+from langchain_core.prompts import ChatPromptTemplate   # structured reusable template with placeholders
+from langchain_core.output_parsers import StrOutputParser    # strips metadata from LLMs
 
-import streamlit as st
-import os
-from dotenv import load_dotenv
+import streamlit as st    # to create web app
+import os   # operating system
+from dotenv import load_dotenv   # to load variables from .env file
 
-os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
-## Langmith tracking
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")   # grabbing api key which is an environment variable
+## LangSmith tracking 
+os.environ["LANGCHAIN_TRACING_V2"]="true"   # to start langSmith tracking
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")   # loading langchain api key
 
 ## Prompt Template
 
 prompt=ChatPromptTemplate.from_messages(
     [
-        ("system","You are a helpful assistant. Please response to the user queries"),
-        ("user","Question:{question}")
+        ("system","You are a helpful assistant. Please response to the user queries"),   # instructions for system
+        ("user","Question:{question}")     # actual input
     ]
 )
 
-## streamlit framework
+## streamlit framework -> creating web app
 
 st.title('Langchain Demo With OPENAI API')
-input_text=st.text_input("Search the topic u want")
+input_text=st.text_input("Search the topic u want")   # renders text box
 
 # openAI LLm 
 llm=ChatOpenAI(model="gpt-3.5-turbo")
 output_parser=StrOutputParser()
-chain=prompt|llm|output_parser
+chain=prompt|llm|output_parser    # LCEL(LangChain Expression Language): prompt -> llm -> parser : using pipe
 
-if input_text:
-    st.write(chain.invoke({'question':input_text}))
+if input_text:     # runs only if user entered something 
+    st.write(chain.invoke({'question':input_text}))    # runs the pipeline
